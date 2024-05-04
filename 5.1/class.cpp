0ef46cd5a5cc881fa8a8a806027e96da1c9310a3
yashvitrivedi07 <yashvitrivedi08@gmail.com>
2024-05-04 13:50:49 +0530

    pr3

---
+++ 5.1/class.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+Q.1 Write a Program to perform all basic arithmetic. operations such as +, -, *, and / operations 
+by implementing method overloading using 2 classes.
+- use only one method named calculate() in the child class
+- if you pass 2 arguments, perform division
+- if you pass 3 arguments, perform subtraction
+- if you pass 4 arguments, perform multiplication
+- if you pass 5 arguments, perform addition
+
+Q.2 Write a Program to implement method overriding by following the below-mentioned criteria:- Class Cricket -> Class T20Match
+- Class Cricket -> Class TestMatch
+- Override getTotalOvers() method in both classes
+*/
+
